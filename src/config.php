95efa93759a84f1db967cae597ccea495aec0636
avalmly <146702530+avalmly@users.noreply.github.com>
2023-10-11 12:16:43 +0200

    Update config.php

--- src/config.php
+++ src/config.php
@@ -2,8 +2,8 @@
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'usuariolamp');
-define('DB_USER', 'usuariolamp');
-define('DB_PASSWORD', 'lamp_password');
+define('DB_USER', 'lamp_db');
+define('DB_PASSWORD', 'usuariolamp');
 
 $mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
